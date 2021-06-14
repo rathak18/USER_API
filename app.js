8ecdc5ea2e19b8fe3a6d656351ca08753ef9bc8a
Rajendra Kr Pathak <rajendrapathak1988@gmail.com>
2021-06-14 08:12:05 +0530

    added server file

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config')
+
+
+
+// imports routes
+app.use(bodyParser.json());
+const postsRoutes = require('./routes/posts')
+
+
+
+app.use('/posts',postsRoutes);
+
+
+
+//ROUTES
+
+app.get("/",(req,res) => {
+    res.send("WE ARE ON HOME");
+})
+
+//  connect to database
+mongoose.connect(process.env.DB_CONNECTION, 
+{useNewUrlParser : true, useUnifiedTopology : true},
+ () => console.log("conncected to db")
+);
+
+// How to listen to server;
+
+app.listen(3000);
